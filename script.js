e8738d45abe0f4235b343d0bdc02390cc5fa6008
cruzado0527-alt <cruzado0527@gmail.com>
2026-01-29 20:56:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function copiarNumero(){
+  navigator.clipboard.writeText("3232124495");
+  alert("Número copiado. Abre Nequi o Daviplata y pega.");
+}
+
+const audio = document.getElementById("ambiente");
+
+document.addEventListener("visibilitychange", () => {
+  if (document.hidden) audio.pause();
+});
+
+window.addEventListener("beforeunload", () => {
+  audio.pause();
+});
+
 const productos = [
   {
     nombre: "Pestilencia – Las Nuevas Aventuras de / La Muerte un Compromiso de Todos",
@@ -29,24 +44,30 @@ const productos = [
   }
 ];
 
-const numeroPago = "573232124495"; // tu número
-
+const numeroPago = "573232124495";
 const contenedor = document.getElementById("productos");
 
 productos.forEach(disco => {
   const card = document.createElement("div");
   card.className = "card";
 
+  const disponible = disco.stock > 0;
+
   card.innerHTML = `
     <img src="${disco.imagen}">
     <h2>${disco.nombre}</h2>
     <p>${disco.descripcion}</p>
     <span>$${disco.precio} COP</span>
-    <strong>Quedan: ${disco.stock}</strong>
+    <strong>${disponible ? "Quedan: " + disco.stock : "SOLD OUT"}</strong>
 
-    <a class="pay" href="https://wa.me/${numeroPago}?text=Quiero%20comprar%20${encodeURIComponent(disco.nombre)}">WhatsApp</a>
-    <a class="pay" href="nequi://send?phone=${numeroPago}&amount=${disco.precio}">Nequi</a>
-    <a class="pay" href="daviplata://send?phone=${numeroPago}&amount=${disco.precio}">Daviplata</a>
+    ${disponible ? `
+      <a class="pay" href="https://wa.me/${numeroPago}?text=Quiero%20comprar%20${encodeURIComponent(disco.nombre)}">WhatsApp</a>
+      <a class="pay" href="nequi://send?phone=${numeroPago}&amount=${disco.precio}">Nequi</a>
+      <a class="pay" href="daviplata://send?phone=${numeroPago}&amount=${disco.precio}">Daviplata</a>
+      <button class="pay" onclick="copiarNumero()">Copiar número</button>
+    ` : `
+      <a class="pay" href="soldout.html">Ver estado</a>
+    `}
   `;
 
   contenedor.appendChild(card);
